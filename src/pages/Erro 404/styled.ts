e8aa62f62aa60d85ad6211ec6382e945a266fca2
Eduardofp <edardofernandespinheiro@gmail.com>
2023-03-14 18:36:10 -0300

    fix: fixing a language bug
    Translating the message from english to pt-br

--- src/pages/Erro 404/styled.ts
+++ src/pages/Erro 404/styled.ts
@@ -4,8 +4,9 @@ export const Main = styled.section`
   max-width: 700px;
   display: flex;
   flex-direction: column;
-  gap: 60px;
   margin: auto;
+  gap: 20px;
+  padding: 10px;
 `;
 export const IMG = styled.img`
   width: 210px;
